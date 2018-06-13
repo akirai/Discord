41987265e4e652e8045b8150aefcca68170a4cdd
akirai <mrkirakyun@yahoo.co.jp>
2018-06-14 02:04:10 +0900

    こんばんは追加

--- testbot/discordbot.py
+++ testbot/discordbot.py
@@ -11,12 +11,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("おはよう")or("ohayo")or("Good Morning"):
+    if message.content.startswith('おはよう'):
         # 送り主がBotだった場合反応しない
         if client.user != message.author:
             # メッセージを書く
             m = "おはようございます" + message.author.name + "さん！"
             # メッセージが送られてきたチャンネルへメッセージを送る
+            await client.send_message(message.channel, m)
+    elif message.content.startswith('こんばんは'):
+        if client.user != message.author:
+            m = "こんばんは" + message.author.name + "さん！"
             await client.send_message(message.channel, m)
 
 client.run("NDU2NDI0MjE2NjMyMjI5ODk5.DgKWXQ.gXLEuZ5dNi587jZYl709UECzVIQ")
